05ac7f01aa7c47fe82aa039daa32290a28983e84
Sourabh Singh Bhadouriya <sourabhsinghbhadouriya10102000@gmail.com>
2023-06-04 17:54:39 +0530

    update

--- Dsa solutions/Graph/3.cpp
+++ Dsa solutions/Graph/3.cpp
@@ -20,3 +20,55 @@
         }
         return cnt;
 }
+
+// by adjacency list
+ //dfs traversal
+    void dfs(int node, vector<int> &visited, vector<vector<int>> &adj)
+    {
+        visited[node] = 1;
+        for (auto neighbour : adj[node])
+        {
+            if (!visited[neighbour])
+            {
+                dfs(neighbour, visited, adj);
+            }
+        }
+    }
+
+    //solve function
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+        //provinces
+        int province = 0;
+
+        //total nodes
+        int n = isConnected.size();
+
+        //creating adjacency list
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (isConnected[i][j] == 1)
+                {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        //basic dfs traversal
+        vector<int> visited(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i])
+            {   
+                //number of time this will be executed is equal to number of provinces
+                province++;
+                dfs(i, visited, adj);
+            }
+        }
+
+        // returning provinces
+        return province;
+    }
