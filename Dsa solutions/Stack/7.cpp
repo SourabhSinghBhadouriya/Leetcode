e1d13d5939e786200ee02e16daa4d5f3b28d5b06
Sourabh Singh Bhadouriya <sourabhsinghbhadouriya10102000@gmail.com>
2023-06-06 17:01:21 +0530

    commit

---
+++ Dsa solutions/Stack/7.cpp
@@ -0,0 +1,35 @@
+// 1249 Minimum Remove to Make Valid Parentheses
+
+string minRemoveToMakeValid(string s)
+{
+    stack<int> st;
+
+    int n = s.length();
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == ')')
+        {
+            if (st.size() != 0)
+                st.pop();
+            else
+                s[i] = '#';
+        }
+        else if (s[i] == '(')
+            st.push(i);
+    }
+
+    while (st.size() != 0)
+    {
+        s[st.top()] = '#';
+        st.pop();
+    }
+
+    string ans = "";
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != '#')
+            ans += s[i];
+    }
+
+    return ans;
+}
