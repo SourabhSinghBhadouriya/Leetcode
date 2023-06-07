f8e98f175e2cfa02d2d4e3ec5f491a38087c31d7
Sourabh Singh Bhadouriya <sourabhsinghbhadouriya10102000@gmail.com>
2023-06-07 10:21:47 +0530

    commit

--- Dsa solutions/Graph/GFG.cpp
+++ Dsa solutions/Graph/GFG.cpp
@@ -23,4 +23,26 @@ vector<int> bfsOfGraph(int V, vector<int> adj[]) {
     }
 
 
-// 
+// DFS of Graph
+
+void dfs(int node, vector<int> adj[], int vis[], vector<int> &ans) {
+        vis[node] = 1; 
+        ans.push_back(node); 
+        // traverse all its neighbours
+        for(auto it : adj[node]) {
+            // if the neighbour is not visited
+            if(!vis[it]) {
+                dfs(it, adj, vis, ans); 
+            }
+        }
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        int vis[V] = {0}; 
+        int start = 0;
+        // create a list to store dfs
+        vector<int> ans; 
+        // call dfs for starting node
+        dfs(start, adj, vis, ans); 
+        return ans; 
+    }
